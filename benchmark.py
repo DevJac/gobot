a2751fa16162d5d3d733066029ca5ae7604adec5
Devin Jacobs <dbuttonz@gmail.com>
2019-04-29 12:38:40 -0600

    Harder benchmark

--- benchmark.py
+++ benchmark.py
@@ -2,7 +2,7 @@ from gobot import Board, Pos
 
 
 def bench_valid_moves():
-    for board_string in open('benchmark_board.txt'):
+    for board_string in open('benchmark_boards.txt'):
         board_string = board_string.strip()
         board = Board.from_state_string(board_string)
         board.valid_moves(Pos.Black)
